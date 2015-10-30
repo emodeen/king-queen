65dab86edef283c9b7728de7ae8415057895bcdc
emodeen <emodeen@gmail.com>
2015-10-29 20:36:47 -0400

    On click, call slideDown() instead of show()

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 		}
 
 		else {
-			$("#slide-down").show();
+			$("#slide-down").slideDown();
 			$("#politics-drop").hide();
 			$("#business-drop").hide();
 			$("#technology-drop").hide();

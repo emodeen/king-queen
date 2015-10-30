5019ac0028aef3bb57e469f7229542012a42875d
emodeen <emodeen@gmail.com>
2015-10-29 20:09:21 -0400

    On click again, hide drop down

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
 
 	$(".international").on("click",function(){
-		$("#slide-down").show();
-		$("#politics-drop").hide();
-		$("#business-drop").hide();
-		$("#technology-drop").hide();
-		$("#culture-drop").hide();
-		$("#blogs-drop").hide();
 
-		$(this).css("background", "#666");
+		if ($(".international").hasClass("active")) {
+			
+			$(".international").removeClass("active");
+			$("#slide-down").hide();
+			console.log("has class active");
+		}
+
+		else {
+			$("#slide-down").show();
+			$("#politics-drop").hide();
+			$("#business-drop").hide();
+			$("#technology-drop").hide();
+			$("#culture-drop").hide();
+			$("#blogs-drop").hide();
+
+			$(this).css("background", "#666");
+
+			$(".international").addClass("active");			
+		}
+
+
 	});
 });

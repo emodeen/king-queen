a59bf541df8a0043b769c0bfc117aef4f08385e7
emodeen <emodeen@gmail.com>
2015-10-30 20:23:08 -0400

    Added function for politics drop-down

--- js/main.js
+++ js/main.js
@@ -5,12 +5,10 @@ $(document).ready(function(){
 		event.preventDefault();
 
 		if ($(".international").hasClass("active")) {
-			
 			$(".international").removeClass("active");
 			$("#slide-down").slideUp();
 			$(this).css("background", "#FFF");
 		}
-
 		else {
 			$("#slide-down").slideDown();
 			$("#politics-drop").hide();
@@ -18,12 +16,30 @@ $(document).ready(function(){
 			$("#technology-drop").hide();
 			$("#culture-drop").hide();
 			$("#blogs-drop").hide();
-
 			$(this).css("background", "#666");
-
 			$(".international").addClass("active");			
 		}
-
-
 	});
+
+	$("#page-header .politics").on("click",function(event){
+
+		event.preventDefault();
+
+		if ($(".politics").hasClass("active")) {
+			$(".politics").removeClass("active");
+			$("#slide-down").slideUp();
+			$(this).css("background", "#FFF");
+		}
+		else {
+			$("#slide-down").slideDown();
+			$("#international-drop").hide();
+			$("#business-drop").hide();
+			$("#technology-drop").hide();
+			$("#culture-drop").hide();
+			$("#blogs-drop").hide();
+			$(this).css("background", "#666");
+			$(".politics").addClass("active");			
+		}
+	});
+
 });

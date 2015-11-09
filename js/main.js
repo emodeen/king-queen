9b23c590fc9db2e947f7e2fd04ede8273c3965b0
emodeen <emodeen@gmail.com>
2015-11-09 17:30:41 -0500

    Show cards by default. Removed start1()

--- js/main.js
+++ js/main.js
@@ -11,48 +11,6 @@ var numQueens = 0;
 // Boolean indicating if the user has clicked 'Start'
 var gameStarted = false;
 
-function start1(event) {
-		event.preventDefault();
-		
-		$("#primary-nav .international").siblings().removeClass("active");
-		$("#primary-nav .international").siblings().css("background", "FFF");
-		$("#primary-nav .international").toggleClass("active");
-
-		// If the nav item is active
-		if ($("#primary-nav .international").hasClass("active")) {
-			// If content is open
-			if($('#slide-down').hasClass('open')) { 
-
-				// .drop.open finds elements within #slide-down that have class drop and class open
-				// Hide all other sections, and remove class open for all other sections 
-				$('#slide-down .drop.open').hide().removeClass('open');
-
-				// Add open class to all elements having both class drop and class international
-				$('.drop.international').show().addClass('open');
-			} 
-
-			// If the content is not open
-			else { 
-
-				// Hide sections with class drop
-				$('#slide-down .drop').hide();
-
-				// Add class open to international section
-				$('.drop.international').show().addClass('open');
-
-				// Slide down content, and add class open to the section div
-				$('#slide-down').slideDown(150).addClass('open');			
-			}
-		}
-
-		// If the nav item is not active		
-		else {
-			$("#slide-down").slideUp().removeClass('open');
-			
-			$('#slide-down .drop.open').removeClass('open');
-		}
-}
-
 function start() {
 	gameStarted = true;
 
@@ -186,7 +144,7 @@ function turn(event) {
 }
 
 $(document).ready(function() {
-	$("#primary-nav .international").on("click",start1);
+	$('.drop.international').show();
 	$(".card").on("click",turn);
 	$("#start").on("click",start);
 	$("#reset").on("click",reset);
